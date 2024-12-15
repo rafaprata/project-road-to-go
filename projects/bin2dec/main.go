1129552832669de1a0b2711c392625e14b9b3972
Rafael Prata <rafaelpratasouza@gmail.com.br>
2024-12-15 22:53:58 +0000

    Add bonus feature

--- projects/bin2dec/main.go
+++ projects/bin2dec/main.go
@@ -7,7 +7,7 @@ import (
 
 func readInput() string {
 	var input string
-	fmt.Println("Enter an up to 8 digit binary number:")
+	fmt.Println("Enter a binary number:")
 	_, err := fmt.Scanln(&input)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -20,9 +20,9 @@ func readInput() string {
 func verifyInput(input string) bool {
 	valid := true
 	switch {
-	case inputLen(input):
-		fmt.Println("Error: The binary number is greater than 8 digits")
-		valid = false
+	// case inputLen(input):
+	// 	fmt.Println("Error: The binary number is greater than 8 digits")
+	// 	valid = false
 	case inputChar(input):
 		fmt.Println("Error: Input is not a binary number")
 		valid = false
@@ -30,9 +30,9 @@ func verifyInput(input string) bool {
 	return valid
 }
 
-func inputLen(input string) bool {
-	return len(input) > 8
-}
+// func inputLen(input string) bool {
+// 	return len(input) > 8
+// }
 
 func inputChar(input string) bool {
 	var charNotValid bool
@@ -48,31 +48,31 @@ func inputChar(input string) bool {
 	return charNotValid
 }
 
-func completeInput(input string) string {
-	for len(input) < 8 {
-		input = "0" + input
-	}
-	return input
-}
+// func completeInput(input string) string {
+// 	for len(input) < 8 {
+// 		input = "0" + input
+// 	}
+// 	return input
+// }
 
-func convertToDec(binaryNumber string) uint8 {
-	var decNumber, intChar uint8
+func convertToDec(binaryNumber string) int {
+	var decNumber, intChar int
 	for k, character := range binaryNumber {
-		index := 7 - k
+		index := (len(binaryNumber) - 1) - k
 		switch {
 		case character == '1':
 			intChar = 1
 		default:
 			intChar = 0
 		}
-		decNumber += intChar * uint8(math.Pow(2, float64(index)))
+		decNumber += intChar * int(math.Pow(2, float64(index)))
 	}
 	return decNumber
 }
 
 func main() {
 	binaryInput := readInput()
-	binaryInput = completeInput(binaryInput)
+	// binaryInput = completeInput(binaryInput)
 	decNumber := convertToDec(binaryInput)
 	fmt.Println(decNumber)
 }
